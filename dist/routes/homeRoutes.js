ea69ecb4fa0509b4bdcd70714c3c6c727a0b3a98
MatheusPereira <matheusp.dasilva@hotmail.com>
2022-12-09 15:47:49 -0300

    erro home

--- dist/routes/homeRoutes.js
+++ dist/routes/homeRoutes.js
@@ -1,8 +1,8 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _express = require('express');
-var _homeController = require('../controllers/homeController'); var _homeController2 = _interopRequireDefault(_homeController);
+var _HomeController = require('../controllers/HomeController'); var _HomeController2 = _interopRequireDefault(_HomeController);
 
 const router = new (0, _express.Router)();
 
-router.get('/', _homeController2.default.index);
+router.get('/', _HomeController2.default.index);
 
 exports. default = router;
