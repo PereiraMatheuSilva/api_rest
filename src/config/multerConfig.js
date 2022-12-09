ed65e09715c01b34eb27b2888f502cf622e83e57
MatheusPereira <matheusp.dasilva@hotmail.com>
2022-12-09 15:14:10 -0300

    Adicionando arquivos

---
+++ src/config/multerConfig.js
@@ -0,0 +1,20 @@
+import { match } from 'assert';
+import multer from 'multer';
+import { extname, resolve } from 'path';
+
+export default {
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
+      return cb(new multer.MulterError('Arquivo precisa ser PNG ou JPG'));
+    }
+    return cb(null, true);
+  },
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, resolve(__dirname, '..', '..', 'uploads', 'images'));
+    },
+    filename: (req, file, cb) => {
+      cb(null, `${Date.now()}_${extname(file.originalname)}`);
+    },
+  }),
+};
