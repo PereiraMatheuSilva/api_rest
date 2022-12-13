dfd2c8c64373e94d302280b24ac34522d11523aa
MatheusPereira <matheusp.dasilva@hotmail.com>
2022-12-12 22:25:49 -0300

    Adicionado cors4

--- dist/app.js
+++ dist/app.js
@@ -7,14 +7,15 @@ var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
 var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
 
 var _homeRoutes = require('./routes/homeRoutes'); var _homeRoutes2 = _interopRequireDefault(_homeRoutes);
-var _userRoutes = require('./routes/userRoutes'); var _userRoutes2 = _interopRequireDefault(_userRoutes);
-var _tokenRoutes = require('./routes/tokenRoutes'); var _tokenRoutes2 = _interopRequireDefault(_tokenRoutes);
+var _UserRoutes = require('./routes/UserRoutes'); var _UserRoutes2 = _interopRequireDefault(_UserRoutes);
+var _TokenRoutes = require('./routes/TokenRoutes'); var _TokenRoutes2 = _interopRequireDefault(_TokenRoutes);
 var _alunoRoutes = require('./routes/alunoRoutes'); var _alunoRoutes2 = _interopRequireDefault(_alunoRoutes);
 var _fotoRoutes = require('./routes/fotoRoutes'); var _fotoRoutes2 = _interopRequireDefault(_fotoRoutes);
 
 const whiteList = [
+  'https://react1.otaviomiranda.com.br',
+  'https://react2.otaviomiranda.com.br',
   'http://localhost:3000',
-  'http://http://35.247.231.243/',
 ];
 
 const corsOptions = {
@@ -44,8 +45,8 @@ class App {
 
   routes() {
     this.app.use('/', _homeRoutes2.default);
-    this.app.use('/users/', _userRoutes2.default);
-    this.app.use('/tokens/', _tokenRoutes2.default);
+    this.app.use('/users/', _UserRoutes2.default);
+    this.app.use('/tokens/', _TokenRoutes2.default);
     this.app.use('/alunos/', _alunoRoutes2.default);
     this.app.use('/fotos/', _fotoRoutes2.default);
   }
