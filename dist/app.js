85cd1984818a64503903efa28d3c820a54ac3ee7
MatheusPereira <matheusp.dasilva@hotmail.com>
2022-12-12 22:35:34 -0300

    cors liberado

--- dist/app.js
+++ dist/app.js
@@ -12,21 +12,6 @@ var _TokenRoutes = require('./routes/TokenRoutes'); var _TokenRoutes2 = _interop
 var _alunoRoutes = require('./routes/alunoRoutes'); var _alunoRoutes2 = _interopRequireDefault(_alunoRoutes);
 var _fotoRoutes = require('./routes/fotoRoutes'); var _fotoRoutes2 = _interopRequireDefault(_fotoRoutes);
 
-const whiteList = [
-  'http://localhost:3000',
-  'http://35.247.231.243/',
-];
-
-const corsOptions = {
-  origin(origin, callback) {
-    if (whiteList.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
-
 class App {
   constructor() {
     this.app = _express2.default.call(void 0, );
@@ -35,7 +20,7 @@ class App {
   }
 
   middlewares() {
-    this.app.use(_cors2.default.call(void 0, corsOptions));
+    this.app.use(_cors2.default.call(void 0, ));
     this.app.use(_helmet2.default.call(void 0, ));
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
