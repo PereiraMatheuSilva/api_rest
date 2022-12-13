62e4c7b2c766e306e316651e0034c26aea7e24d3
MatheusPereira <matheusp.dasilva@hotmail.com>
2022-12-12 23:00:55 -0300

    cors liberado2

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ class App {
 
   middlewares() {
     this.app.use(cors());
-    this.app.use(helmet({ crossOriginOpenerPolicy: true }));
+    this.app.use(helmet({ crossOriginOpenerPolicy: false }));
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
