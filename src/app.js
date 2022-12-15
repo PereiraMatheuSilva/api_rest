62bea12c4197e14d3df3e07084be81da60dafe73
MatheusPereira <matheusp.dasilva@hotmail.com>
2022-12-14 22:10:12 -0300

    teste de novo sei lá

--- src/app.js
+++ src/app.js
@@ -4,12 +4,24 @@ import './database';
 
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/UserRoutes';
 import tokenRoutes from './routes/TokenRoutes';
 import alunoRoutes from './routes/alunoRoutes';
 import fotoRoutes from './routes/fotoRoutes';
+
+const allowlist = ['http://35.247.231.243', 'http://localhost:3000'];
+const corsOptionsDelegate = function (req, callback) {
+  let corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
 
 class App {
   constructor() {
@@ -19,7 +31,8 @@ class App {
   }
 
   middlewares() {
-    this.app.use(cors('*'));
+    this.app.use(cors(corsOptionsDelegate));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
