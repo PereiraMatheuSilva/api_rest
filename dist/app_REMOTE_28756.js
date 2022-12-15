369f87704922b95e9b7dbcfb039bf1e562090afa
Matheus Pereira <matheusp.dasilva@hotmail.com>
2022-12-15 11:24:26 +0000

    sei lá só guardei

---
+++ dist/app_REMOTE_28756.js
@@ -0,0 +1,50 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _path = require('path');
+
+require('./database');
+
+var _express = require('express'); var _express2 = _interopRequireDefault(_express);
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
+
+var _homeRoutes = require('./routes/homeRoutes'); var _homeRoutes2 = _interopRequireDefault(_homeRoutes);
+var _UserRoutes = require('./routes/UserRoutes'); var _UserRoutes2 = _interopRequireDefault(_UserRoutes);
+var _TokenRoutes = require('./routes/TokenRoutes'); var _TokenRoutes2 = _interopRequireDefault(_TokenRoutes);
+var _alunoRoutes = require('./routes/alunoRoutes'); var _alunoRoutes2 = _interopRequireDefault(_alunoRoutes);
+var _fotoRoutes = require('./routes/fotoRoutes'); var _fotoRoutes2 = _interopRequireDefault(_fotoRoutes);
+
+const allowlist = ['http://35.247.231.243', 'http://localhost:3000'];
+const corsOptionsDelegate = function (req, callback) {
+  let corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+
+class App {
+  constructor() {
+    this.app = _express2.default.call(void 0, );
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(_cors2.default.call(void 0, corsOptionsDelegate));
+    this.app.use(_helmet2.default.call(void 0, ));
+    this.app.use(_express2.default.urlencoded({ extended: true }));
+    this.app.use(_express2.default.json());
+    this.app.use('/images/', _express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads', 'images')));
+  }
+
+  routes() {
+    this.app.use('/', _homeRoutes2.default);
+    this.app.use('/users/', _UserRoutes2.default);
+    this.app.use('/tokens/', _TokenRoutes2.default);
+    this.app.use('/alunos/', _alunoRoutes2.default);
+    this.app.use('/fotos/', _fotoRoutes2.default);
+  }
+}
+
+exports. default = new App().app;
